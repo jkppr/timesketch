883700462b678d42d3a6582aed14e997f9582697
janosch <jkpr@google.com>
2023-09-15 15:42:50 +0000

    Update analyzer timelineChip design.

--- timesketch/frontend-ng/src/components/Analyzer/TimelineChip.vue
+++ timesketch/frontend-ng/src/components/Analyzer/TimelineChip.vue
@@ -14,11 +14,32 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <template>
-  <v-chip class="timeline-chip" :style="getTimelineStyle()"
+  <v-chip
+  class="mr-2 mb-3 pr-1 timeline-chip"
+  :class="$vuetify.theme.dark ? 'dark-highlight' : 'light-highlight'"
+  :style="getTimelineStyle()"
   @click:close="$emit('click:close')"
   :close="close"
   >
-    <span class="timeline-name-ellipsis">{{ timeline.name }}</span>
+    <div class="chip-content">
+      <v-icon left :color="timelineChipColor" size="26" class="ml-n2"> mdi-circle </v-icon>
+      <v-tooltip bottom :disabled="timeline.name.length < 30" open-delay="300">
+        <template v-slot:activator="{ on: onTooltip, attrs }">
+          <span
+            class="timeline-name-ellipsis"
+            v-bind="attrs"
+            v-on="onTooltip"
+            >{{ timeline.name }}</span
+          >
+        </template>
+        <span>{{ timeline.name }}</span>
+      </v-tooltip>
+      <span class="right">
+        <span class="events-count" x-small>
+          {{ eventsCount | compactNumber }}
+        </span>
+      </span>
+    </div>
   </v-chip>
 </template>
 
@@ -39,30 +60,49 @@ export default {
     sketch() {
       return this.$store.state.sketch
     },
+    timelineChipColor() {
+      if (!this.timeline.color.startsWith('#')) {
+        return '#' + this.timeline.color
+      }
+      return this.timeline.color
+    },
+    eventsCount: function () {
+      return this.timeline.datasources[0].total_file_events
+    },
   },
   methods: {
     getTimelineStyle() {
       return {
-        'background-color': this.normalizeBgColor(this.timeline.color),
-      };
+        backgroundColor: this.$vuetify.theme.dark ? '#282828' : '#ededed',
+        cursor: 'pointer',
+      }
     },
-    normalizeBgColor(bgColor) {
-      return bgColor.startsWith('#') ? bgColor : `#${bgColor}`;
-    }
   },
 }
 </script>
 
 <style scoped lang="scss">
 .timeline-chip {
-  text-decoration: 'none';
-  opacity: '100%';
+  .right {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .chip-content {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    width: 300px;
+  }
 }
-.theme--dark.timeline-chip {
-  filter: grayscale(25%);
-  color: #333333;
-}
-.timeline-chip .theme--dark.v-icon{
-  color: #333333;
+
+// TODO: Fix the close icon/button margin right!
+// .v-chip__close.v-icon.v-icon--right {
+//   margin-right: 0 !important;
+// }
+
+.events-count {
+  font-size: 0.8em;
 }
 </style>
